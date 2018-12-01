f70433891f4ab9911956b1147db1202f25083115
marcuspousette <marcuspousette@gmail.com>
2018-12-01 17:47:29 +0100

    welcome template done

--- imports/ui/components/welcome/welcome.js
+++ imports/ui/components/welcome/welcome.js
@@ -1,1 +1,17 @@
 import "./welcome.html";
+
+Template.welcome.onCreated( () => {
+
+});
+
+Template.welcome.onRendered( () => {
+
+});
+
+Template.welcome.helpers({
+
+});
+
+Template.welcome.events({
+  
+});
